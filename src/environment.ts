889db0e872424a43c82b805f04950f46f5999191
Гужов Артем <artem.guzhovevg@gmail.com>
2023-01-24 15:23:53 +0700

    feat: setup environment

---
+++ src/environment.ts
@@ -0,0 +1,23 @@
+import * as env from 'env-var'
+import * as dotenv from 'dotenv'
+
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const loadEnv = dotenv.parse(readFileSync(join(__dirname, '..', '.env')))
+
+Object.assign(process.env, loadEnv)
+
+export const environment = {
+    app: {
+        host: env.get('HOST').required().default('localhost').asString(),
+        port: env.get('PORT').required().default('3000').asPortNumber(),
+    },
+    database: {
+        host: env.get('PSQL_HOST').required().default('localhost').asString(),
+        port: env.get('PSQL_PORT').required().default('3000').asPortNumber(),
+        name: env.get('PSQL_DATABASE').required().asString(),
+        username: env.get('PSQL_USERNAME').required().asString(),
+        password: env.get('PSQL_PASSWORD').required().asString(),
+    },
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { environment } from 'environment'
+import { Logardian } from 'logardian'
 import { AppModule } from './app.module'
 
+const logger = new Logardian()
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    const {
+        app: { host, port },
+    } = environment
+
+    logger.configure({})
+
+    const app = await NestFactory.create(AppModule, { logger })
+
+    await app.listen(port, host, () => logger.log(`Server running at http://${host}:${port}`))
 }
 bootstrap()
